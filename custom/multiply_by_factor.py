06983df8d06533661b864f175c3291ea474727b8
Luaithrenn Dempsey <luaidemp@ie.ibm.com>
2019-06-06 21:53:36 +0100

    update

--- custom/multiply_by_factor.py
+++ custom/multiply_by_factor.py
@@ -20,20 +20,18 @@ PACKAGE_URL = 'git+https://github.com/luaithrenn/functions@'
 class MultiplyByFactor(BaseTransformer):
     
     '''
-        Multiply a list of input data items by a constant to produce a data output column
-        for each input column in the list.
+        Multiply an input data items by a constant to produce a data output item.
         '''
     
-    def __init__(self, input_items, factor, output_items):
-        self.input_items = input_items
-        self.output_items = output_items
+    def __init__(self, input_item, factor, output_item):
+        self.input_item = input_item
+        self.output_item = output_item
         self.factor = float(factor)
         super().__init__()
     
     def execute(self, df):
         df = df.copy()
-        for i, input_item in enumerate(self.input_items):
-            df[self.output_items[i]] = df[input_item] * self.factor
+        df[self.output_item] = df[input_item] * self.factor
         return df
     
     @classmethod
@@ -41,15 +39,18 @@ class MultiplyByFactor(BaseTransformer):
         # define arguments that behave as function inputs
         inputs = []
         inputs.append(ui.UIMultiItem(
-          name='input_items',
+          name='input_item',
           datatype=float,
-          description="Data items adjust",
-          output_item='output_items',
-          is_output_datatype_derived=True)
-        )
+          description="Data item to adjusta",
+        ))
         inputs.append(ui.UISingle(
           name='factor',
-          datatype=float)
-        )
+          datatype=float
+        ))
         outputs = []
+        outputs.append(ui.UIFunctionOutSingle(
+          name='output_item',
+           datatype=float,
+           description='Adjusted item'
+         ))
         return (inputs, outputs)
--- scripts/local_test_of_function.py
+++ scripts/local_test_of_function.py
@@ -40,8 +40,8 @@ This file will be written to the working directory.
 '''
 from custom.multiply_by_factor import MultiplyByFactor
 fn = MultiplyByFactor(
-        input_item_1 = '0.3',
+        input_items = '0.3',
         factor= '0.9',
-        output_item = 'adjusted_distance')
+        output_items = 'adjusted_distance')
 fn.execute_local_test(db=db,db_schema=db_schema)
 
